e4fd2dec4354e3fdf1dfdea09f6072d8775c317c
Andreas Heiberg <andreas@heiberg.io>
2017-02-19 22:22:45 +0000

    enable schema support and use c++ api directly
    Using the c api casts between classes for no reason. Might as well use the base api directly.

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,7 +1,5 @@
 #include <phpcpp.h>
-#include <graphqlparser/c/GraphQLAst.h>
-#include <graphqlparser/c/GraphQLAstNode.h>
-#include <graphqlparser/c/GraphQLParser.h>
+#include <graphqlparser/GraphQLParser.h>
 #include <graphqlparser/JsonVisitor.h>
 #include <graphqlparser/AstNode.h>
 #include "../generated/ASTToPHPVisitor.cpp"
@@ -17,21 +15,22 @@ public:
     
     Php::Value parse(Php::Parameters &params)
     {
-        struct GraphQLAstNode *ast;
         const char *error;
 
-        ast = graphql_parse_string(params[0], &error);
+        facebook::graphql::ast::Node *ast = facebook::graphql::parseStringWithExperimentalSchemaSupport(
+            params[0], &error
+        ).release();
 
         if (ast == NULL) {
-            graphql_error_free(error);
             throw Php::Exception(error);
+            std::free((void *)error);
         }
 
         ASTToPHPVisitor visitor;
-        ((facebook::graphql::ast::Node *) ast)->accept(&visitor);
+        ast->accept(&visitor);
         Php::Value result = visitor.getResult();
         
-        graphql_node_free(ast);
+        delete ast;
 
         return result;
     }
